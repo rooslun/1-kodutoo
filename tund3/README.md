f4580c97e1674df492edfdecd2a40c9d4a9d67dd
Roos-Marie Lunden <roosml@tlu.ee>
2020-03-08 23:55:46 +0200

    Final clock app

---
+++ tund3/README.md
@@ -0,0 +1,8 @@
+#Kell
+
+## Roos-Marie Lunden
+
+### 
+Saab vahetada taustavärvi, teksti fonti, teksti suurust
+Saab kuulata muusikat
+
--- tund3/clock.js
+++ tund3/clock.js
@@ -1,67 +1,154 @@
 console.log('Fail õigesti ühendatud.');
 
+let changeColorButton;
+let changeFontButton, fontChanged;
+
 let d = new Date();
-let score = 0;
 let day = d.getDay();
 let date = d.getDate();
 let month = d.getMonth();
 let year = d.getFullYear();
-let days = ['Pühapäev', 'Esmaspäev', 'Teisipäev', 'Kolmapäev', 'Neljapäev', 'Reede', 'Laupäev'];
-let months = ['Jaanuar', 'Veebruar'];
 
-let time = document.querySelector('#time');
-let dayContainer = document.querySelector('#weekday');
-let dateContainer = document.querySelector('#date');
-let monthContainer = document.querySelector('#month');
+let days = [
+  'Pühapäev',
+  'Esmaspäev',
+  'Teisipäev',
+  'Kolmapäev',
+  'Neljapäev',
+  'Reede',
+  'Laupäev'
+];
+let months = [
+  'Jaanuar',
+  'Veebruar',
+  'Märts',
+  'Aprill',
+  'Mai',
+  'Juuni',
+  'Juuli',
+  'August',
+  'September',
+  'Oktoober',
+  'November',
+  'Detsember'
+];
+
+let timeContainer = document.querySelector('#timeContainer');
+let buttonContainer = document.querySelector('#buttonContainer');
+
 let yearContainer = document.querySelector('#year');
-let scoreContainer = document.querySelector('#score');
+let monthName = document.querySelector('#monthName');
 
-scoreContainer.addEventListener('mouseover', changePosition);
-
-upDateClock();
-
-
-
+yearContainer.innerHTML = year;
 
 window.setInterval(upDateClock, 1000);
 
-function upDateClock(){
-    d = new Date();
-    let seconds = d.getSeconds();
-    let minutes = d.getMinutes();
-    let hours = d.getHours();
-    if(seconds < 10){
-        seconds = "0" + seconds;
-    }
-    if(minutes < 10){
-        minutes = "0" + minutes;
-    }
-    if(hours < 10){
-        hours = "0" + hours;
-    }
-    time.innerHTML = hours + ':' + minutes + ':' + seconds;
-    //time.innerHTML = d.toLocaleTimeString('et-EE');
-    //console.log(d);
+window.onload = function() {
+  upDateClock();
+  changeBackgroundColor();
+  changeColorButton = document.querySelector('#change-color');
+  changeColorButton.addEventListener(
+    'click',
+    this.changeBackgroundColor,
+    this.changeDivColor
+  );
+
+  changeFontButton = document.querySelector('#change-size');
+  changeFontButton.addEventListener('click', changeFont);
+  fontChanged = 0;
+
+  changeFontSize = document.querySelector('#change-font');
+  changeFontSize.addEventListener('click', changeTextSize);
+  sizeChanged = 0;
+};
+
+function upDateClock() {
+  d = new Date();
+  let seconds = d.getSeconds();
+  let minutes = d.getMinutes();
+  let hours = d.getHours();
+
+  if (seconds < 10) {
+    seconds = '0' + seconds;
+  }
+  if (minutes < 10) {
+    minutes = '0' + minutes;
+  }
+  if (hours < 10) {
+    hours = '0' + hours;
+  }
+
+  time.innerHTML = hours + ':' + minutes + ':' + seconds;
+  this.monthName.innerHTML = date + '.' + ' ' + months[month] + ' ' + days[day];
+
+  //time.innerHTML = d.toLocaleTimeString('et-EE');
+  //console.log(d);
 }
 
-let positions = ['flex-start', 'flex-end', 'center'];
+/* let positions = ['flex-start', 'flex-end', 'center'];
 
-function changePosition(){
-    score++;
-    scoreContainer.innerHTML = score;
-    scoreContainer.style.order = Math.round(Math.random()*6)
-    scoreContainer.style.alignSelf = positions[Math.round(Math.random()*2)]
+function changePosition() {
+  score++;
+  scoreContainer.innerHTML = score;
+  scoreContainer.style.order = Math.round(Math.random() * 6);
+  scoreContainer.style.alignSelf = positions[Math.round(Math.random() * 2)];
 
-    dayContainer.innerHTML = days[day];
-    dayContainer.style.order = Math.round(Math.random()*6)
-    dayContainer.style.alignSelf = positions[Math.round(Math.random()*2)]
-    dateContainer.innerHTML = date;
-    dateContainer.style.order = Math.round(Math.random()*6)
-    dateContainer.style.alignSelf = positions[Math.round(Math.random()*2)]
-    monthContainer.innerHTML = months[month];
-    monthContainer.style.order = Math.round(Math.random()*6)
-    monthContainer.style.alignSelf = positions[Math.round(Math.random()*2)]
-    yearContainer.innerHTML = year;
-    yearContainer.style.order = Math.round(Math.random()*6)
-    yearContainer.style.alignSelf = positions[Math.round(Math.random()*2)]
+  dayContainer.innerHTML = days[day];
+  dayContainer.style.order = Math.round(Math.random() * 6);
+  dayContainer.style.alignSelf = positions[Math.round(Math.random() * 2)];
+  dateContainer.innerHTML = date;
+  dateContainer.style.order = Math.round(Math.random() * 6);
+  dateContainer.style.alignSelf = positions[Math.round(Math.random() * 2)];
+  monthContainer.innerHTML = months[month];
+  monthContainer.style.order = Math.round(Math.random() * 6);
+  monthContainer.style.alignSelf = positions[Math.round(Math.random() * 2)];
+  yearContainer.innerHTML = year;
+  yearContainer.style.order = Math.round(Math.random() * 6);
+  yearContainer.style.alignSelf = positions[Math.round(Math.random() * 2)];
+} */
+
+function changeBackgroundColor() {
+  const red = Math.round(Math.random() * 255);
+  const green = Math.round(Math.random() * 255);
+  const blue = Math.round(Math.random() * 255);
+  document.body.style.backgroundColor =
+    'rgb(' + red + ',' + green + ',' + blue + ')';
+}
+
+function changeYearColor() {
+  const red = Math.round(Math.random() * 255);
+  const green = Math.round(Math.random() * 255);
+  const blue = Math.round(Math.random() * 255);
+  this.timeContainer.style.backgroundColor =
+    'rgb(' + red + ',' + green + ',' + blue + ')';
+}
+
+function changeColor(colorValue) {
+  document.body.style.backgroundColor = colorValue;
+}
+
+function changeDivColor(colorValue) {
+  document.timeContainer.style.backgroundColor = colorValue;
+}
+
+function changeFont() {
+  console.log('test');
+  if (fontChanged == 0) {
+    timeContainer.style.fontFamily = 'Arial';
+    fontChanged = 1;
+  } else {
+    timeContainer.style.fontFamily = 'Times New Roman';
+    fontChanged = 0;
+  }
+}
+
+function changeTextSize() {
+  console.log('size');
+  if (sizeChanged == 0) {
+    timeContainer.style.fontSize = 'xx-large';
+    sizeChanged = 1;
+  } else {
+    timeContainer.style.fontSize = 'small';
+    sizeChanged = 0;
+  }
 }
